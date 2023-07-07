13d8995ffe9168978dfc2fdcd23e3c3b8387dce4
Erick Rivera <yomellamoerick.771@gmail.com>
2023-07-06 21:50:35 -0400

    the reactions update pdf edition

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,15 @@
 class Product < ApplicationRecord
     has_and_belongs_to_many :categories
-    has_many :reactions #Esto primero
+    has_many :reactions, dependent: :destroy #Esto primero #El dependent fue hecho mucho despues
     has_many :users, through: :reactions #Como base para declarar esto
-    
     # Esto es transitivo
+
+    def count_with_kind(arg)
+        number = self.reactions.where(kind: arg).count
+        return "#{arg} - #{number}"
+    end
+
+    def find_kind_user_relation(user)
+        self.reactions.find_by(user_id: user.id).kind
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   }
   resources :products
   resources :categories
+
+  post '/reactions', to: 'reactions#user_reaction', as: 'user_reaction'
+  get '/my_reactions', to: 'reactions#product_with_reactions', as: 'my_reactions' #NO ES JUSTO YO TENGO EL MONOPOLIO SOBRE LOS NOMBRES MY
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
